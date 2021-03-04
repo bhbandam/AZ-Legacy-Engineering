0b99282c8057afcc7e1744e1a1153d20fb767774
bhbandam <63252764+bhbandam@users.noreply.github.com>
2021-03-04 13:08:06 -0500

    Update wgrvadda.cbl

--- WoodgroveBank/Source Code/CICS3270Pgms/wgrvadda.cbl
+++ WoodgroveBank/Source Code/CICS3270Pgms/wgrvadda.cbl
@@ -1,12 +1,10 @@
       *****************************************************************
-	  ** Copyright (c) Microsoft Corporation.                         * 
-	  ** Copyright (c) Microsoft Corporation.                         *
-	  ** Licensed under the MIT license.                              * 
-	  **                                                              *
-	  ** THIS PROGRAM IS A SAMPLE CICS CLIENT FOR DEMONSTRATING A 3270*
+      ** Copyright (c) Microsoft Corporation.                         * 	
+      ** Licensed under the MIT license.                              * 
+      ** THIS PROGRAM IS A SAMPLE CICS CLIENT FOR DEMONSTRATING A 3270*
       ** APPLICATION THAT READS AND WRITE TO A VSAM DATA SET FOR      * 
       ** BANKING TYPE OF INFORMATION.                                 *
-      **                                                               *
+      **                                                              *
       ** THE INPUT TO THIS CICS PROGRAM IS PROVIDED THROUGH A BMS MAP *
       ** THAT IS NAMED WGRVMAP.                                       *
       *****************************************************************
